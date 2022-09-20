18116834fc4667d664270098332545964d38692d
Andrey <vedischev76@gmail.com>
2022-09-20 20:14:43 +0500

    updated task3

--- task3.py
+++ task3.py
@@ -3,10 +3,12 @@
 # [1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
 
 
+
 def nonrepeat(lst):
     new_lst = []
+    
     for i in lst:
-        if i not in new_lst:
+        if lst.count(i) == 1:
             new_lst.append(i)
     return new_lst
 
